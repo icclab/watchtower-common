0043c77cd9e6bb1f51e16da9c7adb95f0d16b19a
Victor Ion Munteanu <victor.munteanu@equillon.ro>
2015-05-06 14:27:25 +0200

    Fix for Coveralls integration

---
+++ src/test/java/watchtower/common/event/EventTest.java
@@ -0,0 +1,12 @@
+package watchtower.common.event;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+@Test
+public class EventTest {
+  public void test() {
+    assertEquals(1, 1);
+  }
+}
